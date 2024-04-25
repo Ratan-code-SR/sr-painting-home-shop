db4932c944b1f104d3540da4d746ba963ca6095e
ratan <ratanchakma68@gmail.com>
2024-04-25 22:09:34 +0600

    first commit

---
+++ src/layout/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            
+            <h1>i am home </h1>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./layout/Root";
+import Error from "./pages/Error";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
